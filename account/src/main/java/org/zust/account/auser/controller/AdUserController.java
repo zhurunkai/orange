47e8c91ad7a6ec5b3a5bde5b1956565dc86e8e3e
游在云里的鲸 <810120882@qq.com>
2021-06-06 21:06:47 +0800

    实体类的修改以及验证码的部分

---
+++ account/src/main/java/org/zust/account/auser/controller/AdUserController.java
@@ -0,0 +1,11 @@
+package org.zust.account.auser.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AdUserController {
+
+}
--- account/src/main/java/org/zust/account/auser/entity/BookUserEntity.java
+++ account/src/main/java/org/zust/account/auser/entity/BookUserEntity.java
@@ -5,25 +5,27 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+
 @Getter
 @Setter
 @Entity
-@Table(name="auser")
-public class AdUserEntity {
+@Table(name="buser")
+public class BookUserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private Integer id;
-    @Column(name="money")
-    private Double money;
     @Column(name="phone")
     private String phone;
-    @Column(name="nickname")
-    private String nickName;
     @Column(name="portrait")
     private String portrait;
-    @Column(name="freeze")
-    private Double freeze;
+    @Column(name="age")
+    private Integer age;
+    @Column(name="nickname")
+    private String nickname;
+    @Column(name="sex")
+    private String sex;
     @Column(name="token")
     private String token;
+
 }
