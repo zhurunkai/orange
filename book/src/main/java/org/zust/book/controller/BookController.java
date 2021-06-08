acac69f40e68326bfa32e15aced5f763ddfd9fd3
Kaik996 <k@zhurunkaideMacBook-Pro.local>
2021-06-08 20:34:38 +0800

    修改zookeeper端口

--- book/src/main/java/org/zust/book/controller/BookController.java
+++ book/src/main/java/org/zust/book/controller/BookController.java
@@ -1,11 +1,13 @@
 package org.zust.book.controller;
 
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.zust.interfaceapi.dto.AdvertisementDto;
 import org.zust.interfaceapi.service.BookService;
+import org.zust.interfaceapi.service.CommonService;
 import org.zust.interfaceapi.utils.ResType;
 
 import javax.persistence.criteria.CriteriaBuilder;
--- common/src/main/java/org/zust/common/controller/CommonController.java
+++ common/src/main/java/org/zust/common/controller/CommonController.java
@@ -1,11 +1,13 @@
 package org.zust.common.controller;
 
 
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.zust.interfaceapi.service.BookService;
 import org.zust.interfaceapi.service.CommonService;
 import org.zust.interfaceapi.utils.ResType;
 
